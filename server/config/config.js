6f14e03de8b19ae63f5e3bb1ee8719f6ab52774e
andy zhu <andy.zhu@citrix.com>
2018-05-27 04:44:25 +0800

    adding support for Azure

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,14 @@
 const env = process.env.NODE_ENV || 'development'
 
+const cosmosDB = 'az-nodo-todos';
+const cosmosDB_key = encodeURIComponent('FLr9V3L2XvOcCzRugx6Z5Hjv39FF6ebSE6mB5lQA7wZUbSCiFu1WUI2JUaOzMt7irVxqUaNN5RswIeF1G2DFbg==');
+const cosmosDB_name = 'ToDos';
+const cosmosDB_port = 10255;
+
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+    //process.env.MONGODB_URI = `mongodb://${cosmosDB}:${cosmosDB_key}@${cosmosDB}.documents.azure.com:${cosmosDB_port}/${cosmosDB_name}?ssl=true`
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var _mongoose = require('mongoose');
 
 _mongoose.Promise = global.Promise;
-
+console.log(process.env.MONGODB_URI);
 _mongoose.connect(process.env.MONGODB_URI);
+
+
 
 module.exports = {_mongoose};
